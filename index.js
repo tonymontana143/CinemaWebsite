c2cea9dab7f4745e4717b1a7d6e21953d5c68ffc
TonyMontana143 <221074@astanait.edu.kz>
2024-01-10 16:12:38 +0600

    da

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-ц
+const express = require('express')
+const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+
+const app = express()
+
+
+async function start(){
+    try {
+    await mongoose.connect('mongodb+srv://iliias:<q1w2e3r4>@cluster0.oibualh.mongodb.net/?retryWrites=true&w=majority')
+    app.listen(PORT,()=>{
+        console.log('Server has been started...')
+    }
+    )
+    }
+    catch(e){
+        console.log(e)
+    }
+
+}
+
+start()
+
+
+
+
