5f1bcdbfaca8e4f5766e69a0c53c82d67f8ade2b
shomyich <220009@astanait.edu.kz>
2024-02-05 22:25:12 +0600

    points had added

--- authController.js
+++ authController.js
@@ -28,9 +28,10 @@ class authController{
             if (candidate){
                 return res.status(400).json({message: "This nickname is busy"})
             }
+            const coins = 0
             const hashPassword = bcrypt.hashSync(password, 7)
             const userRole = await Role.findOne({value: "USER"})
-            const user = new User({username, password: hashPassword, roles: [userRole.value]})
+            const user = new User({username, password: hashPassword, roles: [userRole.value], bestResult: coins})
             await user.save()
             return res.json({message: "User sucsessfully registered"})
         }catch(e){
@@ -62,7 +63,6 @@ class authController{
     async getUsers(req, res){
         try {
             const users = await User.find()
-
             res.json(users)
         }catch(e){
             console.log(e)
--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const {Schema, model} = require('mongoose')
 const User = new Schema({
     username: {type: String, unique: true, required: true},
     password: {type: String,required: true},
+    bestResult: {type: Number, required: true},
     roles: [{type: String, ref: 'Role'}]
 })
 
